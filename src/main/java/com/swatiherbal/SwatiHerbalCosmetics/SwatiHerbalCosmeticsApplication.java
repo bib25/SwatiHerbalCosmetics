80da32131519f883eaaba680fd428e0cbd8a0084
RahulNikradJ <rahulnikrad@gmail.com>
2025-06-14 09:55:29 +0530

    created project sturuture :SHC-1

---
+++ src/main/java/com/swatiherbal/SwatiHerbalCosmetics/SwatiHerbalCosmeticsApplication.java
@@ -0,0 +1,13 @@
+package com.swatiherbal.SwatiHerbalCosmetics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwatiHerbalCosmeticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwatiHerbalCosmeticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swatiherbal/SwatiHerbalCosmetics/SwatiHerbalCosmeticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swatiherbal.SwatiHerbalCosmetics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwatiHerbalCosmeticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

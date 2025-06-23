dcd31e40f37ba37100a355125014ca28bf09c5b9
RahulNikradJ <rahulnikrad@gmail.com>
2025-06-23 11:55:04 +0530

    created all html files and linked with controller

--- src/main/java/com/swatiherbal/SwatiHerbalCosmetics/controller/HomeController.java
+++ src/main/java/com/swatiherbal/SwatiHerbalCosmetics/controller/HomeController.java
@@ -25,5 +25,61 @@ public class HomeController {
 	{
 		return"about";
 	}
+	@GetMapping("/best_sellers")
+	public String best_sellers()
+	{
+		return"best_sellers";
+	}
+
+	@GetMapping("/body")
+	public String body()
+	{
+		return"body";
+	}
+
+
+	@GetMapping("/cart")
+	public String cart()
+	{
+		return"cart";
+	}
+
+
+	@GetMapping("/face")
+	public String face()
+	{
+		return"face";
+	}
+
+	@GetMapping("/gifts")
+	public String gifts()
+	{
+		return"gifts";
+	}
+
+	@GetMapping("/hair")
+	public String hair()
+	{
+		return"hair";
+	}
+
+	@GetMapping("/rewards")
+	public String rewards()
+	{
+		return"rewards";
+	}
+
+	@GetMapping("/skincare_guide")
+	public String skincare_guide()
+	{
+		return"skincare_guide";
+	}
+
+	@GetMapping("/wellness")
+	public String wellness()
+	{
+		return"wellness";
+	}
+
 
 }
